255eeb57460f0f61355fcd3c90e4a0a7c76ed3ac
anatoliy.ovchinnikov <anatoliy.ovchinnikov@sigma.se>
2014-06-26 19:06:28 +0300

    Added f# project. Added small test.

---
+++ CanopyTraining/Program.fs
@@ -0,0 +1,23 @@
+﻿open canopy
+open runner
+open System
+
+
+start firefox
+
+"first test" &&& fun _ ->
+
+    url "http://localhost:4416/#/category/"
+
+    click "#navList_3"
+
+    notDisplayed "#myModal"
+
+    click "#navList_4"
+
+run()
+
+printfn "press [enter] to exit"
+System.Console.ReadLine() |> ignore
+
+quit()
